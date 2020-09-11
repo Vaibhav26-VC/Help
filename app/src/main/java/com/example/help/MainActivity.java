8a5b50f927415b25aa6f7cb4bcf5b88005a33678
JAYDEEP <jaydipbhayani105@gmail.com>
2020-09-11 13:28:41 +0530

    docter help

--- app/src/main/java/com/example/help/MainActivity.java
+++ app/src/main/java/com/example/help/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.help;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +24,24 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(),login.class));
         finish();
 
+    }
+    public void  onBackPressed(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Are you sure eant to do this ?");
+        builder.setCancelable(true);
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
