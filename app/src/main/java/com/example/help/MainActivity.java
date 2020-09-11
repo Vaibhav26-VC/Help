54d14f7b0db3b00abb2981725393b26060f362ec
JAYDEEP <jaydipbhayani105@gmail.com>
2020-09-11 15:20:27 +0530

    docter help

--- app/src/main/java/com/example/help/MainActivity.java
+++ app/src/main/java/com/example/help/MainActivity.java
@@ -1,22 +1,48 @@
 package com.example.help;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends Activity {
-
+    DrawerLayout drawerlayout;
+    Toolbar toolbar;
+    NavigationView navigation;
+    ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpToolbar();
+
+        navigation=findViewById(R.id.navigation);
+        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case  R.id.nav_sign_out:
+                        FirebaseAuth.getInstance().signOut();
+                        startActivity(new Intent(getApplicationContext(),login.class));
+                        finish();
+
+                }
+                return false;
+            }
+        });
+
     }
     //logout
     public void logout(View view){
@@ -25,6 +51,7 @@ public class MainActivity extends Activity {
         finish();
 
     }
+    //dialogbox
     public void  onBackPressed(){
         final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setMessage("Are you sure eant to do this ?");
@@ -43,5 +70,13 @@ public class MainActivity extends Activity {
         });
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+    }
+    private  void  setUpToolbar(){
+        drawerlayout = findViewById(R.id.drawerlayout);
+        toolbar=findViewById(R.id.toolbar);
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerlayout,toolbar,R.string.app_name,R.string.app_name);
+        drawerlayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
     }
 }
